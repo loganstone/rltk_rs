84eea4469f9d31f328b4a00d28f7363f5afaedb7
Herbert Wolverson <herberticus@gmail.com>
2019-12-06 17:20:41 -0600

    First steps of a DOM and downcasting.

--- examples/ex18-gui.rs
+++ examples/ex18-gui.rs
@@ -3,31 +3,40 @@ rltk::add_wasm_support!();
 use rltk::{Console, GameState, Rltk, RGB};
 use rltk::gui::*;
 
-struct State {}
+struct State {
+    gui : Option<UI>
+}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
-        let mut bg = Background::default(ctx);
-        let mut sb = StatusBar::default(ctx);
-        sb.add_child(PlainText::default(ctx, "Alt-X", 1, 0, RGB::named(rltk::RED), RGB::named(rltk::LIGHT_GRAY)));
-        sb.add_child(PlainText::default(ctx, "Exit", 7, 0, RGB::named(rltk::NAVY), RGB::named(rltk::LIGHT_GRAY)));
-        bg.add_child(sb);
-        let mut mb = MenuBar::default(ctx);
-        mb.add_child(PlainText::default(ctx, "F", 1, 0, RGB::named(rltk::RED), RGB::named(rltk::LIGHT_GRAY)));
-        mb.add_child(PlainText::default(ctx, "ile", 2, 0, RGB::named(rltk::NAVY), RGB::named(rltk::LIGHT_GRAY)));
-        bg.add_child(mb);
+        if let Some(gui) = &mut self.gui {
+            ctx.cls();
 
-        let mut window = Window::new(ctx, 10, 10, 50, 30, "This is a window");
-        window.add_child(PlainText::default(ctx, "Body text", 0, 0, RGB::named(rltk::WHITE), RGB::named(rltk::BLACK)));
-        bg.add_child(window);
-        let ui = UI::new(bg);
-        ui.render(ctx);
+            let fps = gui.element_by_id("fps").unwrap();
+            if let Some(fps) = fps.as_any().downcast_mut::<PlainText>() {
+                fps.text = format!("FPS: {}", ctx.fps);
+            }
+
+            gui.render(ctx);
+        } else {
+            let mut ui = UI::new(Background::default(ctx, "bg"));
+            ui.add("bg", MenuBar::default(ctx, "menubar"));
+            ui.add("bg", StatusBar::default(ctx, "statusbar"));
+            ui.add("statusbar", PlainText::default(ctx, "altx", "Alt-X", 1, 0, RGB::named(rltk::RED), RGB::named(rltk::LIGHT_GRAY)));
+            ui.add("statusbar", PlainText::default(ctx, "exit", "Exit | ", 7, 0, RGB::named(rltk::NAVY), RGB::named(rltk::LIGHT_GRAY)));
+            ui.add("statusbar", PlainText::default(ctx, "fps", &format!("FPS: {}", ctx.fps), 16, 0, RGB::named(rltk::NAVY), RGB::named(rltk::LIGHT_GRAY)));
+            ui.add("menubar", PlainText::default(ctx, "F", "F", 1, 0, RGB::named(rltk::RED), RGB::named(rltk::LIGHT_GRAY)));
+            ui.add("menubar", PlainText::default(ctx, "file", "ile", 2, 0, RGB::named(rltk::NAVY), RGB::named(rltk::LIGHT_GRAY)));
+            ui.add("bg", Window::new(ctx, "win", 10, 10, 50, 30, "This is a window"));
+            ui.add("win", PlainText::default(ctx, "body", "Body text", 0, 0, RGB::named(rltk::WHITE), RGB::named(rltk::BLACK)));
+            self.gui = Some(ui);
+        }
     }
 }
 
 fn main() {
     let context = Rltk::init_simple8x8(80, 50, "Hello GUI", "resources");
 
-    let gs: State = State {};
+    let gs: State = State { gui : None };
     rltk::main_loop(context, gs);
 }
