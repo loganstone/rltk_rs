adeacd33f655fce254e5cea5b57a085ab81fdeb2
Herbert Wolverson <herberticus@gmail.com>
2019-12-09 13:35:40 -0600

    A nicer general layout, still problematic.

--- examples/ex18-gui.rs
+++ examples/ex18-gui.rs
@@ -1,65 +1,47 @@
 rltk::add_wasm_support!();
 
-use rltk::{Console, GameState, Rltk, RGB};
+use rltk::{Console, GameState, Rltk, element};
 use rltk::gui::*;
-use rltk::element;
 
 struct State {
-    gui : Option<UI>
+    pub ui : Option<TextUI>
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
-        if let Some(gui) = &mut self.gui {
-            ctx.cls();
+        ctx.cls();
 
-            if let Some(fps) = element!(gui, "fps", StatusText) {
-                fps.text = format!("FPS: {}", ctx.fps);
-            }            
+        if let Some(ui) = &mut self.ui {
 
-            let events = gui.render(ctx);
-            if let Some(body) = element!(gui, "body", StatusText) {
-                body.text = "Body text goes here. One day, it will be pretty.".to_string();
+            let fps_id = *ui.get_id("fps").unwrap();
+            if let Some(fps) = element!(ui, fps_id, StatusBarText) {
+                fps.set_text(format!("FPS: {}", ctx.fps));
             }
-            for e in events.iter().filter(|e| e.widget == "quit") {
-                match e.event_type {
-                    EventType::MouseOver => {
-                        if let Some(body) = element!(gui, "body", StatusText) {
-                            body.text = "Hovering over the exit".to_string();
-                        }
-                    }
-                    EventType::Clicked => {
-                        ctx.quit();
-                    }
-                }
+            let ft_id = *ui.get_id("frametime").unwrap();
+            if let Some(ft) = element!(ui, ft_id, StatusBarText) {
+                ft.set_text(format!("Frame Time: {}", ctx.frame_time_ms));
             }
+
+            ui.render(ctx);
         } else {
-            self.gui = Some(self.build_gui(ctx));
+            let mut ui = TextUI::new(Theme::turbo_vision());
+            ui
+                .add(ctx, WidgetType::ScreenBackground, "background", "")
+                .set_base("background")
+                .add(ctx, WidgetType::StatusBar, "statusbar", "background")
+                .add(ctx, WidgetType::StatusText{text : "FPS: 00".to_string()}, "fps", "statusbar")
+                .add(ctx, WidgetType::StatusText{text : "Frame Time: 00".to_string()}, "frametime", "statusbar");
+            self.ui = Some(ui);
         }
     }    
 }
 
 impl State {
-    fn build_gui(&self, ctx : &mut Rltk) -> UI {
-        let theme = Theme::turbo_vision();
-        let mut ui = UI::new(theme);
-        ui.add(ctx, "bg", "", WidgetType::Background)
-            .set_base("bg")
-            .add(ctx, "menubar", "bg", WidgetType::MenuBar)
-            .add(ctx, "statusbar", "bg", WidgetType::StatusBar)
-            .add(ctx, "quit", "statusbar", WidgetType::StatusButton{ text : "Exit".to_string(), hotkey: "Alt-X".to_string() })
-            .add(ctx, "fps", "statusbar", WidgetType::StatusText{ text: "FPS: N/A".to_string()})
-            .add_explicit("menubar", PlainText::default(ctx, "F", "F", 1, 0, RGB::named(rltk::RED), RGB::named(rltk::LIGHT_GRAY)))
-            .add_explicit("menubar", PlainText::default(ctx, "file", "ile", 2, 0, RGB::named(rltk::NAVY), RGB::named(rltk::LIGHT_GRAY)))
-            .add_explicit("bg", Window::new(ctx, "win", 10, 10, 50, 30, "This is a window"))
-            .add_explicit("win", PlainText::default(ctx, "body", "Body text", 0, 0, RGB::named(rltk::WHITE), RGB::named(rltk::BLACK)));
-        ui
-    }
 }
 
 fn main() {
     let context = Rltk::init_simple8x8(80, 50, "Hello GUI", "resources");
 
-    let gs: State = State { gui : None };
+    let gs: State = State { ui : None };
     rltk::main_loop(context, gs);
 }
--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,11 +1,162 @@
-use crate::{Rltk, Rect};
-mod element;
-pub use element::Element;
-mod widgets;
-pub use widgets::*;
-mod event;
-pub use event::{Event, EventType};
-mod ui;
-pub use ui::{UI, WidgetType};
+use std::collections::HashMap;
+use crate::{Rltk, Rect, RGB, to_cp437, Console};
 mod theme;
-pub use theme::{Theme, GlyphColor, ColorPair};
+pub use theme::*;
+mod widgets;
+pub use widgets::{SolidBackground, StatusBar, StatusBarText};
+use std::any::Any;
+
+pub struct TextUI {
+    element_store : Vec<ElementStorage>,
+    element_keys : HashMap<String, usize>,
+    theme : Theme,
+    base_element : usize
+}
+
+impl TextUI {
+    pub fn new(theme : Theme) -> TextUI {
+        TextUI{
+            element_keys : HashMap::new(),
+            element_store : Vec::new(),
+            theme,
+            base_element : 0
+        }
+    }
+
+    pub fn get_id<S: ToString>(&self, id : S) -> Option<&usize> {
+        self.element_keys.get(&id.to_string())
+    }
+
+    pub fn add_explicit<S : ToString>(&mut self, key: S, element : Box<dyn Element>) -> usize {
+        let parent_id_option = element.get_info().parent;        
+
+        self.element_store.push(ElementStorage::new(element));
+        let vec_key = self.element_store.len()-1;
+        self.element_keys.insert(key.to_string(), vec_key);
+
+        if let Some(parent_id) = parent_id_option {
+            self.element_store[parent_id].add_child(vec_key);
+        }
+
+        vec_key
+    }
+
+    pub fn add<S : ToString>(&mut self, ctx : &mut Rltk, widget : WidgetType, key : S, parent : S) -> &mut Self {
+        let parent_id = self.element_keys.get(&parent.to_string());
+        let parent_v = match parent_id {
+            None => None,
+            Some(pid) => Some(*pid)
+        };
+        self.add_return_id(ctx, widget, key, parent_v);
+        self
+    }
+
+    pub fn add_return_id<S : ToString>(&mut self, ctx : &mut Rltk, widget : WidgetType, key : S, parent : Option<usize>) -> usize {
+        match widget {
+            WidgetType::ScreenBackground => {
+                self.add_explicit(key, SolidBackground::screen_background(ctx, &self.theme, parent))
+            }
+            WidgetType::StatusBar => {
+                self.add_explicit(key, StatusBar::new(ctx, &self, parent))
+            }
+            WidgetType::StatusText{text} => {
+                self.add_explicit(key, StatusBarText::new(&self, parent, text))
+            }
+        }
+    }
+
+    pub fn set_base<S : ToString>(&mut self, id : S) -> &mut Self {
+        let key = self.element_keys.get(&id.to_string()).unwrap();
+        self.base_element = *key;
+        self
+    }
+
+    pub fn render(&self, ctx : &mut Rltk) {
+        self.element_store[self.base_element].render(ctx);
+        self.render_element(ctx, self.base_element);
+    }
+
+    fn render_element(&self, ctx : &mut Rltk, id : usize) {
+        for child_id in self.element_store[id].get_children().iter() {
+            let element = &self.element_store[*child_id];
+            if element.render(ctx) {
+                self.render_element(ctx, *child_id);
+            }
+        }
+    }
+
+    fn get_bounds(&self, id : usize) -> Rect {
+        self.element_store[id].element.get_info().bounds
+    }
+
+    fn get_child_widths(&self, id : usize) -> i32 {
+        self.element_store[id].element.get_child_widths(&self)
+    }
+
+    #[allow(clippy::borrowed_box)]
+    pub fn element_by_id(&mut self, id : usize) -> &mut Box<dyn Element> {
+        &mut self.element_store[id].element
+    }
+}
+
+pub enum WidgetType {
+    ScreenBackground,
+    StatusBar,
+    StatusText{ text : String }
+}
+
+struct ElementStorage {
+    deleted : bool,
+    visible : bool,
+    element : Box<dyn Element>
+}
+
+impl ElementStorage {
+    pub fn new(element : Box<dyn Element>) -> ElementStorage {
+        ElementStorage {
+            deleted : false,
+            visible : true,
+            element
+        }
+    }
+
+    pub fn render(&self, ctx : &mut Rltk) -> bool {
+        if !self.deleted && self.visible {
+            self.element.render(ctx);
+            return true;
+        }
+        false
+    }
+
+    pub fn get_children(&self) -> &[usize] {
+        self.element.get_children()
+    }
+
+    pub fn add_child(&mut self, id : usize) {
+        self.element.add_child(id);
+    }
+}
+
+pub enum Placement {
+    Absolute,
+    Relative
+}
+
+pub struct ElementInfo {
+    pub placement : Placement,
+    pub bounds : Rect,
+    pub parent : Option<usize>,
+    pub children : Vec<usize>
+}
+
+pub trait Element {
+    fn get_info(&self) -> &ElementInfo;
+    fn get_info_mut(&mut self) -> &mut ElementInfo;
+    fn render(&self, _ctx : &mut Rltk) {}
+    fn get_children(&self) -> &[usize] { &self.get_info().children }
+    fn add_child(&mut self, id : usize);
+    fn get_child_widths(&self, _ui : &TextUI) -> i32 { 0 }
+    fn as_any(&mut self) -> &mut dyn Any;
+}
+
+
